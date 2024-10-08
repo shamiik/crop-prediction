974b51109eaec458e18ca0bb2c6b5ed2c6bd33c3
Shamik Dey Deepto <shamikdey7@gmail.com>
2024-10-09 01:35:19 +0600

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def main():
     st.title("Precision Crop Recommendations Using SVM")
     html_temp = """
     <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Crop Prediction ML App </h2>
+    <h2 style="color:white;text-align:center;"> SmartCrop: AI-Powered Crop Recommendations </h2>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
